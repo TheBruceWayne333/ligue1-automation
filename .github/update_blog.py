388f850a5387fd0383542211999b0ea621d1fbea
TheBruceWayne333 <151718792+TheBruceWayne333@users.noreply.github.com>
2026-01-17 12:49:47 -0800

    Update update_blog.py

--- .github/update_blog.py
+++ .github/update_blog.py
@@ -1,1 +1,12 @@
+# This is inside your update_blog.py on GitHub
+new_post_body = {
+    'title': 'Ligue 1 Matchday Update',
+    'content': YOUR_HTML_CODE_HERE  # This is where your long HTML/CSS string goes
+}
 
+# The "Push" command to connect to Blogger
+service.posts().patch(
+    blogId='YOUR_BLOG_ID', 
+    postId='YOUR_POST_ID', 
+    body=new_post_body
+).execute()
